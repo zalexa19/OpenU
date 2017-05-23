861d3acdb551f4a2e877bf40938b9cbd19942380
OpenU student <student@ubuntu.(none)>
2017-05-23 17:37:27 -0400

    added documentation for lists.c

--- 20465 - Programing in C Lab/Maman12/lists.c
+++ 20465 - Programing in C Lab/Maman12/lists.c
@@ -1,10 +1,42 @@
 /*
  ============================================================================
- Name        : Maman12.c
- Author      : 
+ Name        : Maman12
+ Author      : Alex Zablotsky, 314494964
  Version     :
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : This is Question #2: Implementation of a double linked list.
+
+ Each node is a struct, which consists of an array in the size of 10, a counter (which
+ holds the number of the last populated cell) and two pointers: previous node and next node.
+
+ main:
+Main has two pointers: head and tail of the list. Both are null at the beginning.
+
+ addToList:
+ Receives a reference to the head of the list, tails of the list and the new character.
+ When adding a new char to the array:
+ - Check if the list is empty
+ 	 - If so, create a new node, add the char into the chars array, make head and tail
+ 	   pointers to point to the new node
+     - Otherwise, check the tail's counter. if there's free space in the array,
+       add the char.
+       	   - Otherwise, create a new node, insert the char into index 0, make the tail->
+       	     to point to the new node, the new node->prev to point to the tail, and then
+       	     make the new node as the new tail
+
+
+ printList:
+ Receives the head of the list (a copy).
+ Prints the whole array char by char. Once done, we do the same for the next node.
+ Function stops printing when head becomes NULL.
+
+ freeList:
+ Receives a pointer to first node in the list.
+ A temporary pointer points to the head of the list.
+ The head pointer then points to the next in the link, so we can safely free the first
+ node in the list, which the temp pointer is pointing to.
+ Doing the same for each node until head points to NULL and then we know that the list is empty.
+
  ============================================================================
  */
 
@@ -100,12 +132,10 @@ void addToList (nodeRef * head, nodeRef * tail, int newChar){
 }
 
 void printList(nodeRef head){
-	printf("Printing\n");
 
 	while (head != NULL){
 		int i=0;
 		int arrayInd=head->counter;
-		/*printf("\nCLast used index=%d\n",arrayInd);*/
 
 		for (i=0;i<=arrayInd;i++){
 			printf("%c",head->charArray[i]);
