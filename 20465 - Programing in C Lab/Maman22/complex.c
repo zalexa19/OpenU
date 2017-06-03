29f0439326b0b2cc593e34ee9839a68a62d087ea
OpenU student <student@ubuntu.(none)>
2017-06-03 17:38:55 -0400

    added add_comp

--- 20465 - Programing in C Lab/Maman22/complex.c
+++ 20465 - Programing in C Lab/Maman22/complex.c
@@ -17,21 +17,35 @@ void read_comp(double newA, double newB, complex * name ){
 	name->b=newB;
 }
 
+
+
+/*
+ * This method prints the complex number
+ *
+ */
+
 void print_comp(complex comp){
 	double a=comp.a;
 	double b=comp.b;
 
-	printf("%g",a);
+	printf("(%g ",a);
 
 	if (b<0){
 		printf("%g",b);
 	}
 	else {
-		printf("+%g",b);
+		printf("+ %g",b);
 	}
-	printf("i\n");
+	printf(")i\n");
 
+}
 
+complex add_comp(complex number, complex other_number){
+	complex result;
+	result.a=number.a+other_number.a;
+	result.b=number.b+other_number.b;
+
+	return result;
 }
 
 
