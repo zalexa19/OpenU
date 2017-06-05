9555c04ad5b15e71aaad0d51b26c04872938bdd5
alexz <zalexa19@gmail.com>
2017-06-05 14:08:36 -0700

    random note

--- 20465 - Programing in C Lab/Maman22/project/src/main.c
+++ 20465 - Programing in C Lab/Maman22/project/src/main.c
@@ -13,16 +13,25 @@
 #include <string.h>
 #include "complex.h"
 #define MAXLINELENGTH 100
+#define INITIALMATRIXSIZE 2
 typedef char * array;
 
+
+void print_matrix(array *, int);
 void print_line(array);
-array removeBlanks(char []);
+array fix_spaces(char []);
 array read_user_input();
+array * create_substrings(array);
+array copy_sub_string(char src[], int point1, int point2);
+
+
+
 
 int main() {
 	int close_indicator=0;
 	array orig_line;
 	array trimmed_line;
+	array * matrix;
 
 	complex A,B,C,D,E,F;
 	A.a=0;
@@ -38,13 +47,14 @@ int main() {
 	F.a=0;
 	F.b=0;
 
+
 	printf("\nTrimming spaces tests\n");
 	printf("Test#1\n");
 	orig_line=read_user_input();
 	print_line(orig_line);
-	trimmed_line=removeBlanks(orig_line);
-	printf(trimmed_line);
-
+	trimmed_line=fix_spaces(orig_line);
+	print_line(trimmed_line);
+/*
 	printf("\n\nTest#2\n");
 	orig_line=read_user_input();
 	print_line(orig_line);
@@ -57,6 +67,22 @@ int main() {
 	print_line(orig_line);
 	trimmed_line=removeBlanks(orig_line);
 	print_line(trimmed_line);
+
+	printf("\n\nTest#4\n");
+	orig_line=read_user_input();
+	print_line(orig_line);
+	trimmed_line=removeBlanks(orig_line);
+	print_line(trimmed_line);
+
+	printf("\n\nTest#5\n");
+	orig_line=read_user_input();
+	print_line(orig_line);
+	trimmed_line=removeBlanks(orig_line);
+	print_line(trimmed_line);
+*/
+	printf("\n\nTesting Split\n");
+	matrix = create_substrings(trimmed_line);
+	print_matrix(matrix,INITIALMATRIXSIZE);
 
 
 
@@ -79,6 +105,9 @@ int main() {
 	printf("\n*** END ***\n");
 	return 0;
 }
+
+
+
 
 void print_line(array line){
 	int i=0;
@@ -124,26 +153,37 @@ array read_user_input(){
 }
 
 
-array removeBlanks(array orig){
+array fix_spaces(array orig){
+
 	int orig_size=strlen(orig);
 	array result = (char *)malloc(orig_size);
 	int i;
 	int counter=0;
+	int allowed_space=0;
+	int t=0;
+	char c;
 
-	/*printf("Line size is:%d\n",orig_size);*/
+	c=orig[t];
+	while ((c == ' ') ||(c == '\t')){
+		t++;
+		c=orig[t];
+	}
 
-	for (i=0;i<orig_size;i++){
+	/*t holds a new char*/
+
+	for (i=t;i<orig_size;i++){
 		char c=orig[i];
 
 		if (c ==' '){
-			if ( (counter != 0) && (result[counter-1] != ' ') ){
+			if ( (counter != 0) && (result[counter-1] != ' ') && (allowed_space==0) ){
 				result[counter]=c;
 				counter++;
+				allowed_space=1;
 			}
 		}
 
 		else if (c =='\t'){
-			if ( (i != 0) && (result[counter-1] != ' ') ){
+			if ( (i != 0) && (result[counter-1] != ' ') && allowed_space==0 ){
 				result[counter]=' ';
 				counter++;
 			}
@@ -156,6 +196,11 @@ array removeBlanks(array orig){
 
 		}
 
+		/*else if (c==','){
+			result[counter]= ' ';
+			counter++;
+		}*/
+
 		else{
 			result[counter]=c;
 			counter++;
@@ -167,3 +212,119 @@ array removeBlanks(array orig){
 	return result;
 }
 
+
+/*Split the line into substrings*/
+
+/*
+array * create_substrings(array line){
+	int i;
+	array * matrix;
+	int mat_size=4;
+	int size=strlen(line);
+	array sub;
+	int point1=0;
+	int point2=0;
+	char c;
+	int mat_counter=0;
+	matrix=(array *) malloc(mat_size* sizeof(array));
+
+	for(i=0;i<=size;i++){
+		c=line[i];
+		if (c==' '){
+			point2=i;
+
+			sub=(array)malloc(point2*sizeof(char));
+			memmove(sub,line,point2);
+			matrix[mat_counter]=sub;
+			mat_counter++;
+
+			point1=point2;
+		}
+
+
+		if (c==','){
+			point2=i;
+			sub=copy_sub_string(line,point1,point2);
+		}
+
+	}
+
+	return matrix;
+}
+
+*/
+
+
+
+array * create_substrings(array line){
+	int line_size=strlen(line); /*Line length for creation of a regular array*/
+	int n; /*Used as an index for a loop*/
+	array * matrix; /*Final array*/
+	int i=0; /*Used as an index in the matrix*/
+	array token; /*used for splitting*/
+	int token_size; /*used for calculating size when allocating memory for substrings*/
+	char line_copy[line_size+1]; /*Temporary array for strtok*/
+
+	/*Making a copy of the array*/
+	for (n=0;n<line_size;n++){
+		line_copy[n]=line[n];
+	}
+
+
+
+	matrix=(array*)malloc (INITIALMATRIXSIZE*sizeof(array));
+	printf("%s\n",line_copy);
+
+	token=strtok(line_copy," ,");
+	while (token != NULL){
+		token_size=strlen(token);
+		matrix[i]=(array)malloc(sizeof(char)*token_size);
+		matrix[i]=token;
+		i++;
+		token=strtok(NULL, " ,");
+	}
+
+
+	return matrix;
+
+}
+
+
+
+
+
+
+
+void print_matrix(array * matrix, int size){
+	int i;
+
+	for (i=0;i<size;i++){
+		int in_size=strlen(matrix[i]);
+		int j;
+
+		for (j=0;j<=in_size;j++){
+			printf("%c", matrix[i][j]);
+		}
+
+	}
+
+
+}
+
+array copy_sub_string(char src[], int point1, int point2){
+	int size=(point2-point1)+2;
+	int i;
+
+	array result=(array)malloc(size*sizeof(char));
+	if (!result){
+		printf("issue with memory\n");
+		exit(0);
+	}
+
+	for (i=0;i<size;i++){
+		result[i]=src[i];
+	}
+
+	result[i]='\0';
+	return result;
+}
